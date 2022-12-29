ed0d4606c55f028a3f6a8d329a31e01929b32996
DenLeon <leonden0046@gmail.com>
2022-12-29 14:45:19 +0200

    added firts character difference

--- Laba8/Laba8.cpp
+++ Laba8/Laba8.cpp
@@ -6,36 +6,68 @@ using namespace std;
 
 const int sentenceSize = BUFSIZ;
 
-void CreateFile(ofstream& firstFile);
-void drawLine(int n = 30, char c = '=');
+void CreateFile(ofstream& firstFile, char fileName[]);
+void CompareFiles(ifstream& readFirstFile, ifstream& readSecondFile);
+void drawLine(int n = 50, char c = '=');
 
 int main()
 {
     drawLine();
     cout << "Creating first file: " << endl;
     ofstream firstFile;
-    CreateFile(firstFile);
+    cout << "Please,enter file name: ";
+    char firstFileName[sentenceSize + 1];
+    cin.getline(firstFileName, sentenceSize);
+    char extension[] = ".txt";
+    strcat(firstFileName, extension);
+    CreateFile(firstFile, firstFileName);
     drawLine();
+
+
     cout << "Creating second file: " << endl;
     ofstream secondFile;
-    CreateFile(secondFile);
+    cout << "Please,enter second file name: ";
+    char secondFileName[sentenceSize + 1];
+    cin.getline(secondFileName, sentenceSize);
+    strcat(secondFileName, extension);
+    CreateFile(secondFile, secondFileName);
     drawLine();
+
+
+    ifstream readFirstFile(firstFileName);
+    ifstream readSecondFile(secondFileName);
+    drawLine();
+    CompareFiles(readFirstFile, readSecondFile);
+   
 }
 
-void CreateFile(ofstream& file)
+void CompareFiles(std::ifstream& readFirstFile, std::ifstream& readSecondFile)
 {
-    cout << "Please,enter file name: ";
-    char fileName[sentenceSize + 1];
-    cin.getline(fileName, sentenceSize);
-    char extension[] = ".txt";
-    strcat(fileName, extension);
+    char c;
+    int counter = 0;
+    if (readFirstFile.is_open() && readSecondFile.is_open())
+    {
+        while (readFirstFile.good() && readSecondFile.good())
+        {
+            if (readFirstFile.get() != readSecondFile.get())
+            {
+                cout << "Position where is the first difference = " << counter << endl;
+                break;
+            }
+            counter++;
+        }
+    }
+}
+
+void CreateFile(ofstream& file, char fileName[])
+{
     file.open(fileName);
     if (!file.is_open())
     {
         cout << "Couldn`t open " << fileName << " file" << endl;
         return;
     }
-    cout << "Please,enter a text to write in "<< fileName << "file: " << endl;
+    cout << "Please,enter a text to write in "<< fileName << " file: " << endl;
     char firstFileText[sentenceSize + 1];
     cin.getline(firstFileText, sentenceSize);
     cout << "File " << fileName << " is successfully opened" << endl;
